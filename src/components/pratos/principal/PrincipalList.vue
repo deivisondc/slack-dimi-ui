265f83660320bfd23908ca69ff945ec0953e9fe2
Deivison Cardoso <deivison.cardoso@gruponewway.com.br>
2019-07-24 00:49:17 -0300

    new route (PrincipalForm)

---
+++ src/components/pratos/principal/PrincipalList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <app-list-layout
+      :title="'Prato Principal'"
+      :sub-title="'Listagem de todos os pratos principais cadastrados'"
+      :data-table="dataTable"
+      :columns="columns"
+      :form-route="formRoute"
+    />
+  </div>
+</template>
+
+<script>
+import ListLayout from '../../layout/ListLayout.vue';
+
+export default {
+  components: {
+    AppListLayout: ListLayout,
+  },
+  data() {
+    return {
+      dataTable: [{}],
+      columns: [],
+      formRoute: '/cadastro/prato/principal/new',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import Cardapio from './components/cardapio/Cardapio.vue';
 import CardapioForm from './components/cardapio/CardapioForm.vue';
+
+import PrincipalList from './components/pratos/principal/PrincipalList.vue';
 import PrincipalForm from './components/pratos/principal/PrincipalForm.vue';
 
 import AcompanhamentoList from './components/pratos/acompanhamento/AcompanhamentoList.vue';
@@ -11,7 +13,8 @@ import SaladaForm from './components/pratos/salada/SaladaForm.vue';
 const routes = [
   { path: '/cardapio', component: Cardapio },
   { path: '/cadastro/cardapio', component: CardapioForm },
-  { path: '/cadastro/prato/principal', component: PrincipalForm },
+  { path: '/cadastro/prato/principal', component: PrincipalList },
+  { path: '/cadastro/prato/principal/new', component: PrincipalForm },
   { path: '/cadastro/prato/acompanhamento', component: AcompanhamentoList },
   { path: '/cadastro/prato/acompanhamento/new', component: AcompanhamentoForm },
   { path: '/cadastro/prato/salada', component: SaladaList },
