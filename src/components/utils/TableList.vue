30ca774f477d97f3a399e64f458f604801c7ebfb
Deivison Cardoso <deivison.cardoso@gruponewway.com.br>
2019-07-23 22:37:57 -0300

    new tableList component

---
+++ src/components/utils/TableList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <el-table :data="dataTable" stripe>
+            <el-table-column 
+                v-for="column in columns" 
+                :key="column.name"
+                :prop="column.value"
+                :label="column.name">
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            dataTable: Array,
+            columns: Array
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
