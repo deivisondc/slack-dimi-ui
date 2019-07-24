a5eee1546a44ed555740779c17dcaa379c2f9f63
Deivison Cardoso <deivison.cardoso@gruponewway.com.br>
2019-07-23 23:07:06 -0300

    new route (saladaList)

---
+++ src/components/pratos/salada/SaladaList.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <app-list-layout
+            :title="'Saladas'"
+            :subTitle="'Listagem de todas as saladas cadastradas'"
+            :dataTable="dataTable"
+            :columns="columns"
+            :formRoute="formRoute">
+
+        </app-list-layout>
+    </div>    
+</template>
+
+<script>
+    import ListLayout from '../../layout/ListLayout';
+
+    export default {
+        data() {
+            return {
+                formRoute: '/cadastro/prato/salada/new',
+                dataTable: [
+                    {
+                        name: 'joao',
+                        address: 'teste xpto'
+                    }
+                ],
+                columns: [
+                    {
+                        name: 'Primeira Coluna',
+                        value: 'name'
+                    }
+                ]
+            }
+        },
+        components: {
+            AppListLayout: ListLayout
+        }
+    }
+</script>
+
+<style scoped>
+    
+</style>
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import Cardapio from './components/cardapio/Cardapio.vue';
 import CardapioForm from './components/cardapio/CardapioForm.vue';
 import PrincipalForm from './components/pratos/principal/PrincipalForm.vue';
 import AcompanhamentoForm from './components/pratos/acompanhamento/AcompanhamentoForm.vue';
+
+import SaladaList from './components/pratos/salada/SaladaList';
 import SaladaForm from './components/pratos/salada/SaladaForm.vue';
 
 export const routes = [
@@ -9,5 +11,6 @@ export const routes = [
   { path: '/cadastro/cardapio', component: CardapioForm },
   { path: '/cadastro/prato/principal', component: PrincipalForm },
   { path: '/cadastro/prato/acompanhamento', component: AcompanhamentoForm },
-  { path: '/cadastro/prato/salada', component: SaladaForm },
+  { path: '/cadastro/prato/salada', component: SaladaList },
+  { path: '/cadastro/prato/salada/new', component: SaladaForm },
 ];
