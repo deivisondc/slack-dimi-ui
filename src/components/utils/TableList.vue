907b98ab33135094cc83a2f65d99203d5b4c7fff
Deivison Cardoso <deivison.cardoso@gruponewway.com.br>
2019-07-27 20:21:40 -0300

    new cell with operation buttons

--- src/components/utils/TableList.vue
+++ src/components/utils/TableList.vue
@@ -14,6 +14,18 @@
           :prop="column.value"
           :label="column.name"
         />
+        <el-table-column
+          v-if="showButtonsCell"
+          label=""
+          width="150">
+          <template slot-scope="scope">
+            <div class="buttons-cell-align-right">
+              <el-button icon="el-icon-edit" class="btn-edit" circle size="small"/>
+              <el-button icon="el-icon-copy-document" class="btn-copy" circle size="small" v-if="showCopyButton"/>
+              <el-button icon="el-icon-delete" class="btn-delete" circle size="small"/>
+            </div>
+          </template>
+        </el-table-column>
       </template>
     </el-table>
   </div>
@@ -30,6 +42,16 @@ export default {
       type: Array,
       required: true,
     },
+    showButtonsCell: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    showCopyButton: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
   },
   computed: {
     showTable() {
@@ -40,5 +62,25 @@ export default {
 </script>
 
 <style scoped>
+  .buttons-cell-align-right {
+    text-align: right
+  }
 
+  .btn-delete:hover {
+    color: white;
+    background-color: #dd6161;
+    border-color: #dd6161;
+  }
+
+  .btn-edit:hover {
+    color: white;
+    background-color: #3a8ee6;
+    border-color: #3a8ee6;
+  }
+
+  .btn-copy:hover {
+    color: white;
+    background-color: #5daf34;
+    border-color: #5daf34;
+  }
 </style>
