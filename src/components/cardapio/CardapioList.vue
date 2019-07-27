d55f416b82472d3116c29c404e94047d641e7e55
Deivison Cardoso <deivison.cardoso@gruponewway.com.br>
2019-07-27 20:44:23 -0300

    adding filter props on CardapioList

--- src/components/cardapio/CardapioList.vue
+++ src/components/cardapio/CardapioList.vue
@@ -5,7 +5,31 @@
     :data-table="dataTable"
     :columns="columns"
     :form-route="formRoute"
-  />
+    :showFilter="true"
+    :filterFunction="teste">
+
+    <template slot="secondaryFilter">
+      <el-row>
+        <el-radio-group v-model="selectedWeekDayFilter">
+          <el-badge :value="1" class="item" type="primary" :hidden="!filterResults">
+            <el-radio-button size="small" label="1">Segunda-feira</el-radio-button>
+          </el-badge>
+          <el-badge :value="1" class="item" type="primary" hidden>
+            <el-radio-button size="small" label="2">Terça-feira</el-radio-button>
+          </el-badge>
+          <el-badge :value="2" class="item" type="primary" :hidden="!filterResults">
+            <el-radio-button size="small" label="3">Quarta-feira</el-radio-button>
+          </el-badge>
+          <el-badge :value="3" class="item" type="primary" :hidden="!filterResults">
+            <el-radio-button size="small" label="4">Quinta-feira</el-radio-button>
+          </el-badge>
+          <el-badge :value="1" class="item" type="primary" hidden>
+            <el-radio-button size="small" label="5">Sexta-feira</el-radio-button>
+          </el-badge>
+        </el-radio-group>
+      </el-row>
+    </template>
+  </app-list-layout>
 </template>
 
 <script>
@@ -17,14 +41,34 @@ export default {
   },
   data() {
     return {
-      dataTable: [{}],
-      columns: [],
+      dataTable: [
+        {
+          weekday: 'Segunda-feira',
+          menuItem: 'Churrasco com medalhão de frango fritas e vinagrete'
+        }
+      ],
+      columns: [
+        {
+          name: 'Prato',
+          value: 'menuItem'
+        },
+      ],
       formRoute: '/cadastro/cardapio/new',
+      selectedWeekDayFilter: 1,
+      filterResults: '',
     };
   },
+  methods: {
+    teste() {
+      console.log(this.selectedWeekDayFilter);
+      this.filterResults = 'asd';
+    }
+  }
 };
 </script>
 
 <style scoped>
-
+  .el-badge >>> sup {
+    right: 20px !important;
+  }
 </style>
