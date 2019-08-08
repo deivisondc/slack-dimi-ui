c472a85dab317a61a6758fdd50ab44894245627c
Deivison Cardoso <deivison.cardoso@gruponewway.com.br>
2019-08-08 00:11:44 -0300

    axios default config

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: 'http://localhost:3000/api',
 });
+
+instance.defaults.headers['Content-Type'] = 'application/json';
 
 export default instance;
