f2ab09e1afd5e6213990660e70ffadbd98697c12
Deivison Cardoso <deivison.cardoso@gruponewway.com.br>
2019-08-11 13:16:44 -0300

    removing console.log

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: process.env.VUE_APP_API_BASE_URL,
 });
-console.log(process.env.VUE_APP_API_BASE_URL);
+
 instance.defaults.headers['Content-Type'] = 'application/json';
 
 export default instance;
